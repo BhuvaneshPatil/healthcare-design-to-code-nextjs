347a5355c6e98e1be3e873fabdca97c264337734
Bhuvanesh Patil <bhuvaneshpatil80642@gmail.com>
2021-09-05 21:36:00 +0530

    added constants and logo

---
+++ constants/data.js
@@ -0,0 +1,96 @@
+import images from "./images";
+const data = {
+	services: [
+		{
+			id: 1,
+			picture: images.searchDoctor,
+			title: "Search Doctor",
+			description:
+				"Choose your doctor from thousands of specialist, general, and trusted hospitals",
+		},
+		{
+			id: 2,
+			picture: images.onlinePharamacy,
+			title: "Online Pharmacy",
+			description:
+				"Buy  your medicines with our mobile application with a simple delivery system",
+		},
+		{
+			id: 3,
+			picture: images.consultation,
+			title: "Consultation",
+			description:
+				"Free consultation with our trusted doctors and get the best recomendations",
+		},
+		{
+			id: 4,
+			picture: images.detailsInfo,
+			title: "Details Info",
+			description:
+				"Free consultation with our trusted doctors and get the best recomendations",
+		},
+		{
+			id: 5,
+			picture: images.emergancyCare,
+			title: "Emergancy Care",
+			description:
+				"You can get 24/7 urgent care for yourself or your children and your lovely family",
+		},
+		{
+			id: 6,
+			picture: images.tracking,
+			title: "Tracking",
+			description: "Track and save your medical history and health data ",
+		},
+	],
+	articles: [
+		{
+			id: 1,
+			picture: images.article1,
+			title: "Disease detection, check  up in the laboratory",
+			description:
+				"In this case, the role of the health laboratory is very important to do a disease detection...",
+		},
+		{
+			id: 2,
+			picture: images.article2,
+			title: "Herbal medicines that are safe for consumption",
+			description:
+				"Herbal medicine is very widely used at this time because of its very good for your health...",
+		},
+		{
+			id: 3,
+			picture: images.article3,
+			title: "Natural care for healthy facial skin",
+			description:
+				"A healthy lifestyle should start from now and also for your skin health. There are some...",
+		},
+	],
+	testimonial: [
+		{
+			id: 1,
+			userImage: images.testinomialAvatar,
+			userName: "Edward Newgate",
+			userSubHeading: "Founder Circle",
+			reviewText:
+				"Our dedicated patient engagement app and web portal allow you to access information instantaneously (no tedeous form, long calls, or administrative hassle) and securely",
+		},
+		{
+			id: 3,
+			userImage: images.testinomialAvatar,
+			userName: "Edward Newgate",
+			userSubHeading: "Founder Circle",
+			reviewText:
+				"Our dedicated patient engagement app and web portal allow you to access information instantaneously (no tedeous form, long calls, or administrative hassle) and securely",
+		},
+		{
+			id: 3,
+			userImage: images.testinomialAvatar,
+			userName: "Edward Newgate",
+			userSubHeading: "Founder Circle",
+			reviewText:
+				"Our dedicated patient engagement app and web portal allow you to access information instantaneously (no tedeous form, long calls, or administrative hassle) and securely",
+		},
+	],
+};
+export default data;
---
+++ constants/images.js
@@ -0,0 +1,41 @@
+// logo
+const logo = require("../assets/images/loho.png");
+
+// hero image
+const heroImage = require("../assets/images/hero.svg");
+
+// services image
+const searchDoctor = require("../assets/images/search-doc.svg");
+const onlinePharamacy = require("../assets/images/on-pharma.svg");
+const consultation = require("../assets/images/consult.svg");
+const detailsInfo = require("../assets/images/details-info.svg");
+const emergancyCare = require("../assets/images/emergancy-care.svg");
+const tracking = require("../assets/images/tracking.svg");
+
+// advantages
+const leadingHealthProvider = require("../assets/images/leading-health.svg");
+const downloadApp = require("../assets/images/down-app.svg");
+
+// testimonial
+const testinomialAvatar = require("../assets/images/testimonial-avatar.png");
+
+// articles
+const article1 = require("../assets/images/article-1.png");
+const article2 = require("../assets/images/article-2.png");
+const article3 = require("../assets/images/article-3.png");
+
+export default {
+	heroImage,
+	searchDoctor,
+	onlinePharamacy,
+	consultation,
+	detailsInfo,
+	emergancyCare,
+	tracking,
+	leadingHealthProvider,
+	downloadApp,
+	article1,
+	article2,
+	article3,
+	testinomialAvatar,
+};
