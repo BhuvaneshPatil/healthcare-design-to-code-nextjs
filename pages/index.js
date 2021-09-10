c9daca27b3131b00cad424c23b0ab24717eea89a
Bhuvanesh Patil <bhuvaneshpatil80642@gmail.com>
2021-09-10 09:30:47 +0530

    small Fixtures

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,23 @@
 import Advantages from "../components/Advantages";
 import Articles from "../components/Articles";
 import Footer from "../components/Footer";
-import HamMenu from "../components/HamMenu";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 import Services from "../components/Services";
 import Testimonials from "../components/Testimonials";
 import data from "../constants/data";
+import Head from "next/head";
 export default function Home(props) {
 	return (
 		<div>
-			<HamMenu />
+			<Head>
+				<title>Trafalgar - You health Partner</title>
+				<meta
+					name="viewport"
+					content="initial-scale=1.0, width=device-width"
+				/>
+				<link rel="shortcut icon" href="/public/favicon.ico" />
+			</Head>
 			<Navbar />
 			{/* Hero Section */}
 			<Hero />
@@ -26,4 +33,9 @@ export default function Home(props) {
 			<Footer />
 		</div>
 	);
+}
+export async function getStaticProps(context) {
+	return {
+		props: {}, // will be passed to the page component as props
+	};
 }
